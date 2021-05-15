fab7893620423262e9268fb5b5d8e29ac4525b02
alagn <navateja.alagam@pega.com>
2021-05-15 13:40:56 +0530

    Updates

---
+++ precache-manifest.8b2fb05cfc17de8957e509790b1c1317.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2fec65e77cc59e5afd5207a45677d5",
+    "url": "/ProgressiveWebApp/index.html"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/ProgressiveWebApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b7d23c28b7d6fbaf7250",
+    "url": "/ProgressiveWebApp/static/css/main.7f2aad3b.chunk.css"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/ProgressiveWebApp/static/js/2.277841f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ProgressiveWebApp/static/js/2.277841f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d23c28b7d6fbaf7250",
+    "url": "/ProgressiveWebApp/static/js/main.b7c4e827.chunk.js"
+  },
+  {
+    "revision": "bc296499013297fcb0dc",
+    "url": "/ProgressiveWebApp/static/js/runtime-main.ead50b7b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ProgressiveWebApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
